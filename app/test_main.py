1dc8d8372ca5cebe01166a6f411fd332e647f750
Ron Phillips <mcspidy@gmail.com>
2025-01-29 19:15:11 -0600

    Solution Fix

--- app/test_main.py
+++ app/test_main.py
@@ -2,27 +2,30 @@ import datetime
 import pytest
 from app.main import outdated_products
 from unittest import mock
-from typing import Any
 
 
 @pytest.mark.parametrize(
-    "products,result",
+    "products,mock_today_date,result",
     [
         ([{"name": "salmon",
            "expiration_date": datetime.date(2024, 12, 31),
            "price": 600.0}],
+         datetime.date(2025, 1, 3),
          ["salmon"]),
         ([{"name": "chicken",
            "expiration_date": datetime.date(2025, 1, 30),
            "price": 120.0}],
+         datetime.date(2025, 1, 3),
          []),
         ([{"name": "duck",
            "expiration_date": datetime.date(2025, 2, 3),
            "price": 160.0}],
+         datetime.date(2025, 1, 3),
          []),
         ([{"name": "tuna",
            "expiration_date": datetime.date(2024, 12, 31),
            "price": 95.0}],
+         datetime.date(2025, 1, 3),
          ["tuna"]),
         ([{"name": "beef",
            "expiration_date": datetime.date(2025, 1, 30),
@@ -36,14 +39,16 @@ from typing import Any
           {"name": "horse",
            "expiration_date": datetime.date(2020, 7, 30),
            "price": 950.0}],
+         datetime.date(2025, 1, 3),
          ["crab", "horse"]),
     ],
 )
 @mock.patch("app.main.datetime")
 def test_outdated_products(
-    mock_today: Any,
-    products: Any,
-    result: Any
+    mock_today: datetime.date,
+    products: list[dict],
+    mock_today_date: datetime.date,
+    result: list[str]
 ) -> None:
-    mock_today.date.today.return_value = datetime.date(2025, 1, 3)
+    mock_today.date.today.return_value = mock_today_date
     assert outdated_products(products) == result
